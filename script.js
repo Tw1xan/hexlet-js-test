0af2f5564b7d39f1909aed053c58044d3fa3e7d4
unknown <tw1xan@yandex.ru>
2022-12-12 22:08:01 +0300

    hexlet 14

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log((70 * (3 + 4)) / (8 + 2));
+
+console.log(0.2 * 0.2);
+
+console.log(Number.MAX_SAFE_INTEGER);
+
+console.log(0.39 * 0.22);
+
+console.log((Infinity + Infinity) / 10);
+
+console.log(NaN + 1 / 100);
